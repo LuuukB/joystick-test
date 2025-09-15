a1d39a1e1d9dd4dbe1016be503c58c226014c5c6
LuuukB <158037306+LuuukB@users.noreply.github.com>
2025-09-15 14:25:10 +0200

    print added

--- libs/virtual_joystick/tank.py
+++ libs/virtual_joystick/tank.py
@@ -7,20 +7,23 @@ os.environ["KIVY_NO_ARGS"] = "1"
 from kivy.uix.widget import Widget
 from kivy.lang.builder import Builder
 from kivy.clock import Clock  # noqa: E402
+from kivy.properties import NumericProperty
 
 class VirtualTankWidget(Widget):
+    percent = NumericProperty(50)
     def __init__(self, **kwargs):
         super(VirtualTankWidget, self).__init__(**kwargs)
 
+
         self.level: float = 0.5
-        self.percent: float = 50.0
 
         Clock.schedule_interval(self.draw_water, 1/30)
 
         Builder.load_file(os.path.join(os.path.dirname(__file__), "res/tank.kv"))
 
-    def set_percent(self, percent: float):
-        self.percent = percent
+    def set_percent(self, nieuw: float):
+        percent = nieuw
+        print(percent)
 
     def adjust_water(self, level: float) -> None:
         self.level = level
@@ -28,6 +31,8 @@ class VirtualTankWidget(Widget):
 
     def draw_water(self, dt: float):
         self.water_level = self.level
+
+
 
 
 
