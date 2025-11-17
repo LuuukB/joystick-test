d1ea545853ef2c4047fde57a20a387d0b0ec6179
LuuukB <158037306+LuuukB@users.noreply.github.com>
2025-11-17 11:00:53 +0100

    verkeerde imports

--- src/factory/camera_factory.py
+++ src/factory/camera_factory.py
@@ -2,25 +2,25 @@
 import asyncio
 from pathlib import Path
 from typing import Dict
-from camera_handler import CameraHandler
-from amiga_camera import AmigaCamera
-from offline_camera import OfflineVideoCamera
+from ..camera.i_camera_handler  import ICameraHandler
+from ..camera.camera_handler import CameraHandler
+from ..camera.offline_camera_handler import OfflineCameraHandler
 
 class CameraFactory:
     """Factory en manager voor meerdere camera's tegelijk."""
 
     def __init__(self):
-        self.cameras: Dict[str, CameraHandler] = {}
+        self.cameras: Dict[str, ICameraHandler] = {}
 
     def add_camera(self, name: str, robot_online : bool, source: str = None, stream_name: str = "rgb"):
         """Voeg een camera toe aan de factory."""
         if robot_online:
-            cam = AmigaCamera(Path(source), stream_name)
+            cam = CameraHandler(Path(source), stream_name)
         else:
-            cam = OfflineVideoCamera(source)
+            cam = OfflineCameraHandler(source)
         self.cameras[name] = cam
 
-    def get_camera(self, name: str) -> CameraHandler:
+    def get_camera(self, name: str) -> ICameraHandler:
         return self.cameras[name]
 
     async def start_all(self):
